ca6621a6410a8f5efdee8f9e72934969d62d42ea
spagna <32439053+spagna@users.noreply.github.com>
2017-10-09 15:11:00 +0200

    Add files via upload

--- Neuron.cpp
+++ Neuron.cpp
@@ -1,58 +1,72 @@
 #include "Neuron.hpp"
 #include <iostream>
-#include <vector>
+#include <cmath>
 
-Neuron::Neuron (double membranePotential, int spikes, std::vector<double> time)
+Neuron::Neuron (double V_membrane, unsigned int nb_spikes, double t_spike, bool refractory)
+: V_membrane_ (V_membrane), nb_spikes_(nb_spikes), t_spike_(t_spike), r_period_(refractory)
 {} 
 
-double Neuron::getMembranePotential() const
+double Neuron::getV_membrane() const
 {
-	return membranePotential_;
+	return V_membrane_;
 }
 
 int Neuron::getNumberSpikes() const
 {
-	return spikes_;
+	return nb_spikes_;
 }
 
-double Neuron::getTimeSpike(size_t i)
+double Neuron::getTimeSpike()
 {
-	return t_[i];
+	return t_spike_;
 }
 
-void Neuron::setMembranePotential(double membranePotential)
+void Neuron::setV_membrane(double V_membrane)
 {
-	membranePotential_ = membranePotential;
+	V_membrane_ = V_membrane;
 	
 }
 
-void Neuron::setNumberSpikes(int spikes)
+void Neuron::setNumberSpikes(unsigned int nb)
 {
-	spikes_ = spikes;
+	nb_spikes_ = nb;
 }
 
-void Neuron::setTimeSpike (size_t i, double t)
+void Neuron::setTimeSpike (double t)
 {
-	t_[i] = t;
+	t_spike_ = t;
 }
 
-void Neuron::update()
+void Neuron::update(double dt)
 {
-	//membranePotential_ = exp(-h
+	double simulation_time (0.0);
+	double R = tau/C; //membrane resistance
+	double external_input (0.0);
+	double const1 =  exp(-h/tau);
+	std::cout << "Choose a value for the external input" << std::endl; // rem: the value should be >=0
+	std::cin >> external_input;
+	simulation_time = t_start;
+	//nb_spikes_ = 0;
+	do {
+		if (V_membrane_ > V_thr){ // if the membrane potential crosses the threshold
+			t_spike_ = simulation_time; //store the time of the spike
+			std::cout << "The spike occurred at: " << t_spike_ << std::endl;
+			r_period_ = true; // the neuron enters in its refractory period
+			++nb_spikes_; //the number of spike increases of one
+		} 
+		if (r_period_ and tau_rp >= simulation_time - t_spike_){ //if the neuron is in its refractory state and it's still in the refractory period
+			V_membrane_ = V_refractory; //the membrane potential is zero
+		} else {
+			V_membrane_ = const1*V_membrane_ + external_input*R*(1-const1); //solve the membrane equation
+		}
+		if (simulation_time - t_spike_ >= tau){ //when the refractory period has finished, the neuron is no more in this state
+			r_period_ = false;
+		}
+		simulation_time += dt;	//the simulation advanced of 
+	} while (simulation_time < t_stop);	
+	std::cout << "Number of spikes: " << nb_spikes_ << std::endl;
 }
 
-void Neuron::addSpikeTime(double t)
-{
-	t_.push_back(t);
-}
-
-bool Neuron::emptyVector() const
-{
-	return t_.empty();
-}
 
 Neuron::~Neuron ()
 {}
-
-
-
--- Neuron.hpp
+++ Neuron.hpp
@@ -2,24 +2,34 @@
 #define NEURON_H
 
 #include <iostream>
-#include <vector>
+#include <fstream>
+
+constexpr double t_start (0.0); //in milliseconds
+constexpr double t_stop (500.0);
+constexpr double tau (20.0); //time constant in milliseconds (note tau = R/C)
+constexpr unsigned int C (1); //number of connections from other neurons
+constexpr double V_thr (20.0); //firing threshold in microvolts 
+constexpr double V_reset (10.0); //reset potential in microvolts
+constexpr double V_refractory (0.0);
+constexpr double tau_rp (2.0); //refractory period in milliseconds
+constexpr double h (0.1); //equidistant step of time during which the simulation evolves
+
 
 class Neuron 
 {
 public:
-	Neuron (double membranePotential = 0.0, int spikes = 0, std::vector<double> time = std::vector<double> (0, 0.0));
+	Neuron (double membranePotential = 0.0, unsigned int nb_spikes = 0, double t_spike = 0.0, bool r_period = false);
 	
-	double getMembranePotential() const;
+	double getV_membrane() const;
 	int getNumberSpikes() const;
-	double getTimeSpike(size_t i); 
+	double getTimeSpike(); 
 	
-	void setMembranePotential(double membranePotential);
-	void setNumberSpikes(int spikes);
-	void setTimeSpike (size_t i, double t);
-	void addSpikeTime(double t); //add a time to the vector
-	bool emptyVector () const;
+	void setV_membrane(double V_membrane);
+	void setNumberSpikes(unsigned int nb);
+	void setTimeSpike (double t);
 	
-	void update();
+	void update(double dt);
+	void storeDatas(std::ofstream file);
 	
 	
 	
@@ -28,10 +38,14 @@ public:
 	
 
 private:
-	double membranePotential_; 
-	int spikes_; //number of spikes
-	std::vector<double> t_; //times when the spikes occured 
+	double V_membrane_; 
+	unsigned int nb_spikes_; //number of spikes
+	double t_spike_; //time when a spike occured
+	bool r_period_; //if true the neuron is in its refractory period
 	
 };
 
 #endif
+
+
+
