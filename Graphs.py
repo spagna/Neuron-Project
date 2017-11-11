fcc6c710e5b06f8bdb51ac61f5d9edb590803849
spagna <sara.pagnamenta.96@gmail.com>
2017-11-11 17:31:09 +0100

    Modified values for the python script

--- Graphs.py
+++ Graphs.py
@@ -10,13 +10,13 @@ data = np.genfromtxt('Spike_time.txt')
 select= np.array([d for d in data if d[1] < 30])
 data1= select.transpose()
 ax1.scatter(0.1*data1[0],data1[1],alpha=0.8, edgecolors='none');
-ax1.set_xlim([0,500])
-ax1.set_ylim([0,35])
+ax1.set_xlim([500,600])
+
 
 select2= np.array([d for d in data])
 data2= select2.transpose()
 ax2 = fig.add_subplot(212)
-n, bins, patches = ax2.hist(0.1*data2[0], 500, normed=0, alpha=0.75)
-ax2.set_xlim([0,500])
-ax2.set_ylim([0,6000])
+n, bins, patches = ax2.hist(0.1*data2[0], 1000, normed=0, alpha=0.75)
+ax2.set_xlim([500,600])
+ax2.set_ylim([0,10000])
 pl.show();
